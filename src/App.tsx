80ddb3e20f4dc49bfb0cac8a51a6194769142129
Tzvetan Marinov <tmarinov@stamus-networks.com>
2024-07-17 17:01:06 +0100

    Type Annotations for useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
-import UserSearch from './classes/UserSearch';
-
-const users = [
-  { name: "Sarah", age: 20 },
-  { name: "Alex", age: 20 },
-  { name: "Michael", age: 20 },
-];
+import UserSearch from './refs/UserSearch';
 
 function App() {
   return (
       <div>
         <h1>Vite + React</h1>
-        <UserSearch users={users} />
+        <UserSearch />
       </div>
   );
 }
---
+++ src/refs/UserSearch.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState, useRef } from "react";
+
+const users = [
+  { name: "Sarah", age: 20 },
+  { name: "Alex", age: 20 },
+  { name: "Michael", age: 20 },
+];
+
+const UserSearch: React.FC = () => {
+  const [name, setName] = useState("");
+  const [user, setUser] = useState<undefined | { name: string; age: number }>();
+  const inputRef = useRef<HTMLInputElement | null>(null); // OR useRef<any>(null) for quick prototyping
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, []);
+
+  const onClick = () => {
+    const foundUser = users.find((user) => {
+      return user.name === name;
+    });
+
+    setUser(foundUser);
+  };
+
+  return (
+    <div>
+      <input
+        ref={inputRef}
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button onClick={onClick}>Find User</button>
+      <div>
+        {user?.name}
+        {user?.age}
+      </div>
+    </div>
+  );
+};
+
+export default UserSearch;
